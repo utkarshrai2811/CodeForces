5671e3dfc391ef994f884bcf5dd19d117fe69af9
Utkarsh Rai <utkarshrai.281100@gmail.com>
2020-04-28 15:27:47 +0530

    added few more questions

--- 703A.cpp
+++ 703A.cpp
@@ -1,5 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
+int a, n, m, mis, cri;
 int main() {
-	
+    cin >> a;
+    for (int i = 0; i < a; i++) {
+        cin >> n >> m;
+        if (n > m) {
+            mis++;
+        } else if(n < m) {
+            cri++;
+        }
+    }
+    if (mis > cri) {
+        cout << "Mishka";
+    } else if (cri > mis) {
+        cout << "Chris";
+    } else {
+        cout << "Friendship is magic!^^";
+    }
 }
--- 996A.cpp
+++ 996A.cpp
@@ -1,5 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
+int n, a, b, c, d, e, sum;
 int main() {
-	
+	cin >> n;
+	a = n/100;
+	n -= a * 100;
+	b = n/20;
+	n -= b * 20;
+	c = n/10;
+	n -= c * 10;
+	d = n/5;
+	n -= d * 5;
+	e = n/1;
+	n -= e * 1;
+	sum = a+b+c+d+e;
+	cout << sum << "\n";
 }
