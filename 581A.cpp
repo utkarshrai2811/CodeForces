4ef7ed2c0369f4f6bd3f9c60445f80ec9312a468
Utkarsh Rai <utkarshrai.281100@gmail.com>
2020-04-28 13:19:24 +0530

    added more questions

--- 581A.cpp
+++ 581A.cpp
@@ -1,5 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
+	int a, b;
+	int counter = 0;
+	int res;
+	cin >> a >> b;
+	while((a!=0) && (b!=0)) {
+		a -= 1;
+		b -= 1;
+		counter += 1;
+	}
+	if(a!=0)
+		res = a / 2;
+	else
+		res = b / 2;
 	
+	cout << counter << " " << res << "\n";
 }
