d1e9d6004c4c1bf59f5b0502516b70cb79665974
Utkarsh Rai <utkarshrai.281100@gmail.com>
2020-04-28 20:52:53 +0530

    added more questions

--- 1097A.cpp
+++ 1097A.cpp
@@ -1,5 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
+	int i;
+    string s, x;
+    cin>>s;
+    for(i=0;i<5;i++) {
+        cin>>x;
+        if(x[0]==s[0] || x[1]==s[1]) {
+            cout<<"YES\n";
+            return 0;
+        }
+    }
+    cout<<"NO";
+	return 0;
 	
 }
--- 935A.cpp
+++ 935A.cpp
@@ -1,5 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
-	
+	int n,m,x,cnt;
+	cin >> n;
+	cnt = 0;
+	x = 1;
+	while(1) {
+		if(x>n-x)
+			break;
+		m=n-x;
+		if(m%x==0)
+			cnt++;
+		x++;
+	}
+	cout << cnt << "\n";
+	return 0;
 }
