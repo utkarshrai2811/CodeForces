3ba30933891b610287756741ab0f91a47ee69e66
Utkarsh Rai <utkarshrai.281100@gmail.com>
2020-04-29 13:00:49 +0530

    Add Files via upload

--- 1223A.cpp
+++ 1223A.cpp
@@ -1,7 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long int ll;
-typedef unsigned long long int ull;
+//typedef long long int ll;
+//typedef unsigned long long int ull;
 int main() {
-	
+	int q;
+	cin>>q;
+	while(q--) {
+		int n;
+		cin >> n;
+		if(n==2)
+			cout<<2<<"\n";
+		else if(n%2==0 && n>2)
+			cout<<0<<"\n";
+		else
+			cout<<1<<"\n";
+		}
+		return 0;
 }
