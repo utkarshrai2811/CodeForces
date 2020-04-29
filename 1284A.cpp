0cb66dad98fd8755a92e796430897e7e6727ab21
Utkarsh Rai <utkarshrai.281100@gmail.com>
2020-04-29 20:31:43 +0530

    Add Files via upload

--- 1284A.cpp
+++ 1284A.cpp
@@ -1,7 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long int ll;
-typedef unsigned long long int ull;
 int main() {
-	
+	int n, m;
+	string s[20], t[20];
+	cin >> n >> m;
+	for(int i=0; i<n; i++) 
+		cin >> s[i];
+	for(int i=0; i<m; i++) 
+		cin >> t[i];
+	int q; 
+	cin >> q;
+	while(q--) {
+		int x; 
+		cin >> x; 
+		x--;
+		cout << s[x % n] + t[x % m] << "\n";
+	}
 }
--- 867A.cpp
+++ 867A.cpp
@@ -1,7 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long int ll;
-typedef unsigned long long int ull;
+//typedef long long int ll;
+//typedef unsigned long long int ull;
 int main() {
-	
+	int n,i,a,b,s=0,f=0;
+	string str;
+	while(cin>>n) {
+		cin>>str;
+		for(i=0; i<n;i++) {
+			if(a == 1 && str[i] == 'S') {
+				++f;
+				a = b = 0;
+			}
+			else if (b == 1 && str[i] == 'F') {
+                ++s;
+                a = b = 0;
+            }
+            
+            if (str[i] == 'F')
+                a = 1;
+            else
+                b = 1;
+               }
+            if(s>f)
+				cout<<"YES"<<"\n";
+			else
+				cout<<"NO"<<"\n";
+		}
+			return 0;
 }
