cde282e1f43dcfd9b90a75fad3535ca03f49d241
Utkarsh Rai <utkarshrai.281100@gmail.com>
2020-04-30 12:31:39 +0530

    Add Files via upload

--- 1281A.cpp
+++ 1281A.cpp
@@ -1,7 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long int ll;
-typedef unsigned long long int ull;
+//typedef long long int ll;
+//typedef unsigned long long int ull;
 int main() {
-	
+	int t,n;
+	string s;
+	cin>>t;
+	while(t--) {
+		cin>>s;
+		n = s.length();
+		if(n >= 2 && s[n - 2] == 'p' && s[n - 1] == 'o')
+            cout<<("FILIPINO")<<"\n";
+        else if(n >= 4 && s[n - 4] == 'd' && s[n - 3] == 'e' && s[n - 2] == 's' && s[n - 1] == 'u')
+			cout<<("JAPANESE")<<"\n";
+        else if(n >= 4 && s[n - 4] == 'm' && s[n - 3] == 'a' && s[n - 2] == 's' && s[n - 1] == 'u')
+			cout<<("JAPANESE")<<"\n";
+        else
+			cout<<("KOREAN")<<"\n";
+		}
+		return 0;
 }
--- 615A.cpp
+++ 615A.cpp
@@ -1,7 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long int ll;
-typedef unsigned long long int ull;
+//typedef long long int ll;
+//typedef unsigned long long int ull;
 int main() {
-	
+        int i,j;
+        int n,x,b,m;
+        map<int,bool> mp;
+
+        cin>>n>>m;
+        for(i=1;i<=m;i++) {       
+            mp[i]=false;
+        }
+        for(i=0;i<n;i++) {
+            cin>>x;
+            for(j=0;j<x;j++) {
+                cin>>b;
+                mp[b]=true;
+            }
+        }
+        for(i=1;i<=m;i++) {
+            if(mp[i]==false)
+            {
+                cout<<"NO";
+                return 0;
+            }
+        }
+
+        cout<<"YES";
+
+        return 0;
 }
