1bfb96b4370cc31186fd2a611efc18b1ec17d959
Utkarsh Rai <utkarshrai.281100@gmail.com>
2020-05-02 06:57:58 +0530

    Add Files via upload

--- 749A.cpp
+++ 749A.cpp
@@ -3,5 +3,25 @@ using namespace std;
 typedef long long int ll;
 typedef unsigned long long int ull;
 int main() {
-	
+	ll n,i;
+	cin>>n;
+	if(n%2) {
+		if(n==3) {
+			cout<<"1"<<"\n"<<"3";
+		}
+	else {
+		n-=3;
+		cout<<(n/2)+1<<"\n";
+		for(i=0; i<n/2; i++)
+			cout<<"2 ";
+		
+		cout<<"3";
+		}
+	}
+	else {
+		cout<<n/2<<"\n";
+		for(i=0; i<n/2; i++)
+			cout<<"2 ";
+		}
+		return 0;
 }
